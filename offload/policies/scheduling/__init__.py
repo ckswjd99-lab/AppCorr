dc5e3596fc3c24676e21dbb1cd0f624cc7fb0ef5
Chanjeong Park <cjpark99@snu.ac.kr>
2026-05-04 17:08:26 +0900

    Implemented instructions, but suspicious

--- offload/policies/scheduling/__init__.py
+++ offload/policies/scheduling/__init__.py
@@ -4,3 +4,4 @@ from .dynamic_group_trigger import DynamicGroupTriggerPolicy
 from .coco_window_trigger import COCOWindowDynamicPolicy, COCOWindowInterleavedPolicy
 from .ade20k_approx_correct import ADE20KApproxCorrectPolicy
 from .ade20k_sequential import ADE20KSequentialPolicy
+from .nyu_approx_correct import NYUApproxCorrectPolicy
